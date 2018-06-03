1c77c290158e3a3eb12cf098d363e5c167cd5669
Andy Philbert Jr <andyphilbertjr@gmail.com>
2018-06-02 22:34:04 -0400

    Update xoCounterAlgProblem.js

--- xoCounterAlgProblem.js
+++ xoCounterAlgProblem.js
@@ -1,7 +1,18 @@
+//Check to see if a string has the same amount of 'x's and 'o's. 
+//The method must return a boolean and be case insensitive. 
+//The string can contain any char.
+
+//Examples input/output:
+//XO("ooxx") => true
+//XO("xooxx") => false
+//XO("ooxXm") => true
+//XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+//XO("zzoo") => false
+
 
 function XO(str) {
 	var strLowerCase = str.toLowerCase()
-  var numOfX = 0
+ 	var numOfX = 0
   var numOfO = 0
   for(let letter = 0; letter<=strLowerCase.length; letter++){
     if (strLowerCase[letter] === 'x'){
