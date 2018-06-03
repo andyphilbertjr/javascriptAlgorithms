81534502ff8a0a5def9ac1a21f7da08620a6140e
Andy Philbert Jr <andyphilbertjr@gmail.com>
2018-06-03 02:25:06 -0400

    Create Binary Converter
    Takes the sum of multiple numbers and coverts it to a binary string

---
+++ binaryNumConv.js
@@ -0,0 +1,27 @@
+//Implement a function that adds two numbers together and returns their sum in binary. 
+//The conversion can be done before, or after the addition.
+//The binary number returned should be a string.
+
+
+
+
+function addBinary(a,b) {
+  let sumOfNum = a + b
+  let binaryNumToConv = sumOfNum%2
+  let divisor = function(currentDivisableNum){
+        sumOfNum = currentDivisableNum/2
+        binaryNumToConv = currentDivisableNum%2
+        return sumOfNum && binaryNumToConv
+  } 
+  let binaryNumArray = []
+  while(sumOfNum >= 1){
+    if(binaryNumToConv.toFixed() >= 1){
+      binaryNumArray.push(1)
+    }else{
+      binaryNumArray.push(0)
+    }
+
+    divisor(sumOfNum)
+  }
+  return binaryNumArray.concat().join('')
+}
