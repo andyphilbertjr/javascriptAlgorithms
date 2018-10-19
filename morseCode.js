4b4ceffaff28e949fbbd3d34b9c595bdc7f2293e
Andy Philbert Jr <andyphilbertjr@gmail.com>
2018-10-19 16:06:29 -0400

    Update morseCode.js

--- morseCode.js
+++ morseCode.js
@@ -14,6 +14,6 @@ decodeMorse = function(morseCode){
   let threeSpace = morseCode.split('   ')
   let conversion = []
   threeSpace.forEach(word => conversion.push(word.split(' ').map( letter  => MORSE_CODE[letter]).join('')))
- return conversion.join(' ')
+ return conversion.join(' ').trim()
  
  }
